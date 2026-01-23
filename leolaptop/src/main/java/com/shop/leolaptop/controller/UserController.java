2a484eab0de99e0880f8fdfcc3b84fe63fd01937
PhamTuanDat <pdat100111@gmail.com>
2026-01-23 21:06:19 +0700

    feat: design interface create user page

--- leolaptop/src/main/java/com/shop/leolaptop/controller/UserController.java
+++ leolaptop/src/main/java/com/shop/leolaptop/controller/UserController.java
@@ -1,18 +1,15 @@
 package com.shop.leolaptop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/admin/user")
 public class UserController {
 
-    @RequestMapping("/home")
-    public String homeView() {
-        return "home";
-    }
-
-    @RequestMapping("/product")
-    public String productView() {
-        return "product-view";
+    @GetMapping
+    public String getCreateUserPage() {
+        return "admin/user/create";
     }
 }
