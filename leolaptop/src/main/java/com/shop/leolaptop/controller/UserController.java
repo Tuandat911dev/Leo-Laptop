63e72a8ab1779dead99cee741ac59d9077c5e092
PhamTuanDat <pdat100111@gmail.com>
2026-01-23 13:56:30 +0700

    feat: config view engine jsp - jstl

---
+++ leolaptop/src/main/java/com/shop/leolaptop/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.shop.leolaptop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UserController {
+
+    @RequestMapping("/home")
+    public String homeView() {
+        return "home";
+    }
+
+    @RequestMapping("/product")
+    public String productView() {
+        return "product-view";
+    }
+}
