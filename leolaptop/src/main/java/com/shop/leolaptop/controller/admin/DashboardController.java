660cef861ea9bd37e510deeb5133301da8b6f05b
PhamTuanDat <pdat100111@gmail.com>
2026-01-27 16:50:50 +0700

    feat: integrate template admin dashboard

---
+++ leolaptop/src/main/java/com/shop/leolaptop/controller/admin/DashboardController.java
@@ -0,0 +1,14 @@
+package com.shop.leolaptop.controller.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin/dashboard")
+public class DashboardController {
+    @GetMapping
+    public String getDashboardPage() {
+        return "admin/dashboard/dashboard";
+    }
+}
